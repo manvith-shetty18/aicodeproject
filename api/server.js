8f388451ac4310ea54167fd72f9435254daa1282
manvith-shetty18 <manviths41@gmail.com>
2025-03-29 23:32:40 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const app = require('./src/app');
 app.use(express.json());
 
 
-const allowedOrigins = ["http://localhost:5173", "https://aicodereviewer.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173", "https://aicodereviewer.vercel.app"];
 
 app.use(cors({
   origin: function (origin, callback) {
